c5ccea8486168e207bb6742d4fb76b25a3467430
Cleonildo Jr <cleu.junior@gmail.com>
2023-10-21 12:07:16 -0600

    feat: Config Eureka Client

--- hr-payroll/src/main/java/br/com/ms/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/ms/hrpayroll/HrPayrollApplication.java
@@ -2,20 +2,17 @@ package br.com.ms.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@EnableCircuitBreaker
-//@EnableEurekaClient
-@RibbonClient(name = "hr-worker")
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class HrPayrollApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrPayrollApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
 
 }
