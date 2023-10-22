896e637aacd1cc3c6716c2aded6ffd812f145d6b
Cleonildo Jr <cleu.junior@gmail.com>
2023-10-22 11:38:24 -0600

    feat: Project hr-config-server created

---
+++ hr-config-server/src/main/java/br/com/ms/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,20 @@
+package br.com.ms.hrconfigserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+    @Value("${spring.cloud.config.server.git.username}")
+    private String username;
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrConfigServerApplication.class, args);
+    }
+
+
+}
