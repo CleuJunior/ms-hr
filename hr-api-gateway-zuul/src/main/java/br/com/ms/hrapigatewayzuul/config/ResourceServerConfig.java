28981b970533b00b7acce3b130e4c8baa3fee6fe
Cleonildo Jr <cleu.junior@gmail.com>
2023-10-24 06:01:36 -0600

    feat: Oauth, Zuul, JWT

---
+++ hr-api-gateway-zuul/src/main/java/br/com/ms/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -0,0 +1,74 @@
+package br.com.ms.hrapigatewayzuul.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    private final JwtTokenStore tokenStore;
+	private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
+	private static final String[] OPERATOR = { "/hr-worker/**" };
+	private static final String[] ADMIN =
+            { "/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/actuator/**", "/hr-oauth/actuator/**" };
+    private static final String ROLE_ADMIN = "ADMIN";
+    private static final String ROLE_OPERATOR = "OPERATOR";
+    private static final String[] ROLES = { ROLE_OPERATOR, ROLE_ADMIN };
+
+    public ResourceServerConfig(JwtTokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    @Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(this.tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+
+		http
+			.authorizeRequests()
+			.antMatchers(PUBLIC).permitAll()
+			.antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole(ROLES)
+			.antMatchers(ADMIN).hasRole(ROLE_ADMIN)
+			.anyRequest().authenticated();
+
+//		http.cors().configurationSource(corsConfigurationSource());
+	}
+//
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration corsConfig = new CorsConfiguration();
+//		corsConfig.setAllowedOrigins(List.of("*"));
+//		corsConfig.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE", "PATCH"));
+//		corsConfig.setAllowCredentials(true);
+//		corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", corsConfig);
+//		return source;
+//	}
+//
+//	@Bean
+//	public FilterRegistrationBean<CorsFilter> corsFilter() {
+//		FilterRegistrationBean<CorsFilter> bean
+//			= new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return bean;
+//	}
+}
